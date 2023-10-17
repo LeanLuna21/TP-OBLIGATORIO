bb427561588f82ea7a12186b547e9c5ffa3b6542
Leandro <109328793+LeanLuna21@users.noreply.github.com>
2023-10-17 15:50:07 -0300

    Sitio TERMINADO ahora si

---
+++ Propieteer Inmoviliaria/JAVASCRIPT/Card-Casa.js
@@ -0,0 +1,71 @@
+// Función boton 1
+//
+document.getElementById("mostrarFormulario").addEventListener("click", function() {
+    var formulario = document.getElementById("FormContainer");
+    formulario.style.display = "inline-block";
+});
+
+// Función boton 2
+//
+document.getElementById("mostrarForm").addEventListener("click", function() {
+    var formulario = document.getElementById("FormContainer");
+    formulario.style.display = "inline-block";
+});
+
+// Función boton 3
+//
+document.getElementById("mostrarForm2").addEventListener("click", function() {
+    var formulario = document.getElementById("FormContainer");
+    formulario.style.display = "inline-block";
+});
+
+// Función boton Cerrar Formulario
+//
+document.getElementById("ocultarForm").addEventListener("click", function() {
+    var formulario = document.getElementById("FormContainer");
+    formulario.style.display = "none";
+});
+
+
+// Función boton "Mas Detalles Card-1"
+//
+document.getElementById("masDetallesC1").addEventListener("click", function() {
+    var formulario = document.getElementById("cuadroDetallesC1");
+    formulario.style.display = "inline-block";
+});
+
+// Función boton Cerrar cuadro Detalles
+//
+document.getElementById("ocultarCuadroC1").addEventListener("click", function() {
+    var formulario = document.getElementById("cuadroDetallesC1");
+    formulario.style.display = "none";
+});
+
+
+// Función boton "Mas Detalles Card-2"
+//
+document.getElementById("masDetallesC2").addEventListener("click", function() {
+    var formulario = document.getElementById("cuadroDetallesC2");
+    formulario.style.display = "inline-block";
+});
+
+// Función boton Cerrar cuadro Detalles
+//
+document.getElementById("ocultarCuadroC2").addEventListener("click", function() {
+    var formulario = document.getElementById("cuadroDetallesC2");
+    formulario.style.display = "none";
+});
+
+// Función boton "Mas Detalles Card-3"
+//
+document.getElementById("masDetallesC3").addEventListener("click", function() {
+    var formulario = document.getElementById("cuadroDetallesC3");
+    formulario.style.display = "inline-block";
+});
+
+// Función boton Cerrar cuadro Detalles
+//
+document.getElementById("ocultarCuadroC3").addEventListener("click", function() {
+    var formulario = document.getElementById("cuadroDetallesC3");
+    formulario.style.display = "none";
+});
